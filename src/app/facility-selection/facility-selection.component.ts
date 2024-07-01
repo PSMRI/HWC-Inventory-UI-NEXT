0e0a5a8cd558d5bbd9ad29125066e6c767a47005
bhpratibha <165631570+bhpratibha@users.noreply.github.com>
2024-07-01 19:53:06 +0530

    Develop to master code push for the CI build configurations (#9)
    * Commit for Develop
    
    * Changes in hwc-inv as functionality
    
    * small changes in env file
    
    * change in index file
    
    * CSS changes
    
    * change in url in ev files
    
    * CSS changes
    
    * Change in index file name
    
    * fix the check box issue in rx
    
    * removed commented code and vun
    
    * ci build configurations changes
    
    * ci environmentfile changes
    
    * Bug fix for upgrad--url
    
    * Bug fix for hwc-inv for upgrad
    
    * CSS changes
    
    ---------
    
    Co-authored-by: Parth Kothari <pa20251871@wipro.com>
    Co-authored-by: Nagendra Babu Kokkiligadda <NA20311191@wipro.com>
    Co-authored-by: Bhallamudi Pratibha <BH20131883@wipro.com>
    Co-authored-by: nkokkiligadda87 <nkokkiligadda87@gmail.com>

--- src/app/facility-selection/facility-selection.component.ts
+++ src/app/facility-selection/facility-selection.component.ts
@@ -70,13 +70,13 @@ export class FacilitySelectionComponent implements OnInit, DoCheck {
     const facility: any = this.facilityForm.controls['facility'].value;
     const subFacility: any = this.facilityForm.controls['subFacility'].value;
     if (
-      this.facilityForm.controls.isMainStore.value &&
+      this.facilityForm.controls.isMainStore.value === 'true' &&
       this.facilityForm.controls.facility.value
     ) {
       this.enableContinue = true;
       localStorage.setItem('facilityID', facility.facilityID);
       localStorage.setItem('facilityDetail', JSON.stringify(facility));
-    } else if (isMainStore == 'false' && facility && subFacility) {
+    } else if (isMainStore === 'false' && facility && subFacility) {
       this.enableContinue = true;
       localStorage.setItem('facilityID', subFacility.facilityID);
       localStorage.setItem('facilityDetail', JSON.stringify(subFacility));
